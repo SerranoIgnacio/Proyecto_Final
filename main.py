bc0e283cf9bf6471bf5a1213920fca6d133e2eb7
Ignacio Serrano <johndoe@example.com>
2021-12-22 22:18:28 -0300

    prueba

--- main.py
+++ main.py
@@ -6,38 +6,65 @@ def menu():
     print('-------------------------------------')
     print('|¡Bienvenido a Corralon de Materiales')
     print('|Con un N° del 1 al 5 selecciona una opción:')
-    print('|(1) ¿Quiere Agregar un Material Nuevo?')
-    print('|(2) ¿Quiere Agregar Stock a un Material?')
-    print('|(3) ¿Quiere consultar Stock de un material?')
-    print('|(4) ¿Quiere Consultar Stock Completo?')
+    print('|(1) ¿Agregar un Material Nuevo?')
+    print('|(2) ¿Agregar Stock a un Material?')
+    print('|(3) ¿Consultar Stock de un material?')
+    print('|(4) ¿Consultar Stock Completo?')
     print('|(5) Salir.')
     print('-------------------------------------')
 
 
-'''if os.path.exists("archivo.csv") == True:
-     # si existe entonces lo abro con "a" en vez de "w"
-else:
-    # lo abro con "w" para crearlo'''
+def agrega_material(material, stock):
+    if os.path.exists("materiales.csv") == True:
+        csvfile = open('materiales.csv', 'a')
+        header = ['material', 'stock'] 
+        data = csv.DictWriter(csvfile, fieldnames=header)
+        fila = {}
+        fila['material'] = material
+        fila['stock'] = stock
+        data.writerow(fila)
+        csvfile.close()
+    else:
+        with open('materiales.csv', 'w', newline='') as csvfile:
+            header = ['material', 'stock'] 
+            writer = csv.DictWriter(csvfile, fieldnames=header)
+            writer.writeheader()
+            fila = {}
+            fila['material'] = material
+            fila['stock'] = stock
+            writer.writerow(fila)
 
-
+        
+def buscar_material():
+    pass
+   
 if __name__ == '__main__':
     p = 0
     while p == 0:
         menu()
-        opcion = input()
+        opcion = input('Opción: ')
         if opcion.isnumeric():
+            opcion = int(opcion)
             if opcion in range(1,6):
-                if opcion == 1:
+                if opcion == 1: #Agregar un Material Nuevo?
+                    material = input('Ingrese nombre del material: ')
+                    stock = input('Ingrese cantidad del material: ')
+                    agrega_material(material, stock)
+                    #material, stock = ''
+                elif opcion == 2: #¿Agregar Stock a un Material?
+                    material = input('ingrese material a incrementar: ')
+                    buscar_material()
+                    #agrega_stock()
                     pass
-                elif opcion == 2:
+                elif opcion == 3: #¿Consultar Stock de un material?
+                    #consulta_stock_material()
                     pass
-                elif opcion == 3:
+                elif opcion == 4: #¿Consultar Stock Completo?
+                    #consulta_stock_completo()
                     pass
-                elif opcion == 4:
-                    pass
-                elif opcion == 5:
-                    p = 1
+                elif opcion == 5: #Salir
                     print('Saliendo del programa...\n')
+                    p = 1
             else:
                 print('¡¡¡Seleccion incorrecta!!! Elija un dato valido.\n')
         else:
